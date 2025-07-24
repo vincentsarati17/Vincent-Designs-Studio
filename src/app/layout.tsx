1fc74de5ceb7d8b1f3191b67e9eeddb63459450b
Vincent Sarati <vincentsarati@gmail.com>
2025-07-24 23:22:40 +0000

    so that mean we are going to reupload the project on github again?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,11 @@ export const metadata: Metadata = {
     "digital agency",
     "Namibia"
   ],
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({

e17746c4e56881cdf3b4f068996fab1f768c4a3c
Vincent Sarati <vincentsarati@gmail.com>
2025-08-25 11:30:59 +0000

    let's take a thorough check for security risks

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  projectId: "vincent-designs",
-  appId: "1:201595097998:web:812ffdc2480d00488a0adf",
-  storageBucket: "vincent-designs.appspot.com",
-  apiKey: "AIzaSyDsWjvw_MGdM0sk7quTLKUTxRmdjVOXVoE",
-  authDomain: "vincent-designs.firebaseapp.com",
-  messagingSenderId: "201595097998",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase

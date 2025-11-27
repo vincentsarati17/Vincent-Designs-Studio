4b491f2675219b1aa62b7da7d5d4d0d637a0032f
Vincent Sarati <vincentsarati@gmail.com>
2025-11-27 09:29:40 +0000

    ⚠ The "middleware" file convention is deprecated. Please use "proxy" ins

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,9 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  skipMiddlewareUrlNormalize: false,
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [

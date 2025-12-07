648171335e78627c59133ca6ee171158a19ebb33
Vincent Sarati <vincentsarati@gmail.com>
2025-12-07 08:36:28 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

--- src/proxy.ts
+++ src/proxy.ts
@@ -73,10 +73,13 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
+     * - api (API routes)
      * - _next/static (static files)
+     * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - VINCEDSTUDIO.icon.png (logo)
+     * - image/ (other images)
      */
-    '/((?!_next/static|favicon.ico|VINCEDSTUDIO.icon.png).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|VINCEDSTUDIO.icon.png|image/).*)',
   ],
 };

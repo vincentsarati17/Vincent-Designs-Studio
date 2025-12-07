688ab9f91e58ec9c0096281561b04d5d1b8a3509
Vincent Sarati <vincentsarati@gmail.com>
2025-12-07 06:33:44 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ next.config.mjs
@@ -0,0 +1,26 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  skipProxyUrlNormalize: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
